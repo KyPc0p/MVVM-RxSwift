f71dfabe2306116942100af64eb0c51d0547b811
Artem Kharchenko <artem.kh4rchenko@ya.ru>
2022-10-20 16:56:57 +0700

    Initial Commit

---
+++ MVVM + RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM + RxSwift
+//
+//  Created by Артём Харченко on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
